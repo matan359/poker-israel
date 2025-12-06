0ce3d8266232cb2ee1e39a2e15c7e612e7d135f6
Matan <matanyou7@gmail.com>
2025-12-06 19:59:07 +0200

    UI: Redesign playing cards to look like real poker cards with realistic styling

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -12,15 +12,31 @@ const Card = (props) => {
     },
     applyFoldedClassname
   } = props;
+  
+  const isRed = suit === 'Diamond' || suit === 'Heart';
+  const suitSymbol = renderUnicodeSuitSymbol(suit);
+  
   return(
     <div 
       key={`${suit} ${cardFace}`} 
-      className={`playing-card cardIn ${(applyFoldedClassname ? ' folded' : '')}`} 
+      className={`playing-card cardIn ${(applyFoldedClassname ? ' folded' : '')} ${isRed ? 'red-suit' : 'black-suit'}`} 
       style={{animationDelay: `${(applyFoldedClassname) ?  0 : animationDelay}ms`}}>
-      <h6
-        style={{color: `${(suit === 'Diamond' || suit === 'Heart') ? 'red' : 'black'}`}}>
-        {`${cardFace} ${renderUnicodeSuitSymbol(suit)}`}
-      </h6>
+      {/* Top corner - value and suit */}
+      <div className="card-corner card-corner-top">
+        <div className="card-value">{cardFace}</div>
+        <div className="card-suit-small">{suitSymbol}</div>
+      </div>
+      
+      {/* Center - large suit symbol */}
+      <div className="card-center">
+        <div className="card-suit-large">{suitSymbol}</div>
+      </div>
+      
+      {/* Bottom corner - value and suit (rotated) */}
+      <div className="card-corner card-corner-bottom">
+        <div className="card-value">{cardFace}</div>
+        <div className="card-suit-small">{suitSymbol}</div>
+      </div>
     </div>
   )
 }
